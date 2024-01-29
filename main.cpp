633f1591594ec067e48038ecacf86955e7e6a9c8
Tristan Mangin <tw.mangin@gmail.com>
2024-01-29 11:24:31 -0500

    Setup Debugging

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    cout << "Welcome to the Economic Calculator!\\ What topic can I help you with today?" << endl;
+    return 0;
+}
