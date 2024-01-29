e5ca0204472bcb108b43fa6a5c0841e3c4ec6586
Tristan Mangin <tw.mangin@gmail.com>
2024-01-29 15:20:36 -0500

    Started Framework

--- main.cpp
+++ main.cpp
@@ -3,11 +3,47 @@
 #include <string>
 
 using namespace std;
+/*
+TOPICS TO INCLUDE
+    Interest Rates
+    Monetary Policy
+    Fiscal Policy
+*/
+void interestRates() {}
 
+void monetaryPolicy() {}
+
+void fiscalPolicy() {}
+
+// main function of the program handles user input and direction
 int main() {
-    string topic;
-    cout << "Welcome to the Economic Calculator!\nWhat topic can I help you with today?" << endl;
-    cin >> topic;
+    string topic = "";
+    int topicNum = -1;
+    bool exit = false;
+    cout << "Welcome to the Economic Calculator!\n\nWhat topic can I help you with today?" << endl;
+
+    // main loop of the program prompts user until they want to exit
+    while (!exit) {
+        cout << "1. Interest Rates\n2. Monetary Policy\n3. Fiscal Policy\n0. Exit\n\n>>";
+        cin >> topic;
+
+        // Make sure the input is valid
+        if (isalpha(topic[0])) { 
+            cout << "\nPlease Select a valid topic\n";
+            continue;
+        }
+        topicNum = stoi(topic);
+
+        // Checks prompt and goes to appropriate method
+        if (topicNum == 0) { exit = true; }
+        else if (topicNum == 1) { interestRates(); }
+        else if (topicNum == 1) { monetaryPolicy(); }
+        else if (topicNum == 1) { fiscalPolicy(); }
+        else {
+            cout << "\nPlease Select a valid topic\n\n";
+        }
+    }
+
 
 
     return 0;
