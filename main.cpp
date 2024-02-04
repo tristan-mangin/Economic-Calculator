06287b8731188a866c1ddd5cf9e222616db94667
Tristan Mangin <tw.mangin@gmail.com>
2024-02-03 21:33:52 -0500

    Started compoundInterest() Implementation

--- main.cpp
+++ main.cpp
@@ -5,29 +5,57 @@
 
 using namespace std;
 
-string COMMANDS[] = {"help", "exit", "ci", "di", "pv", "ytm"};
-
 // Outputs Instructions and the list of commands to the terminal
 void help() {
     cout << "\t1. exit\t- terminate the program\n" <<
     "\t2. ci\t- solve for compounding interest\n" <<
     "\t3. di\t- solve for discounting interest\n" <<
     "\t4. pv\t- solve for present value\n" <<
-    "\t5. ytm\t- solve for yield to maturity\n"<< endl;
+    "\t5. ytm\t- solve for yield to maturity\n" << endl;
 }
 
-void compoundInterest() {}
+/*
+Collects variables and solves for the missing variable in compounding interest problems
+*/
+interestRateProblem compoundInterest() {
+    int solve;
+    bool solving = false;
 
-void discountInterest() {}
+    cout << "What do you need to solve for?" << endl;
 
-void presentValue() {} 
+    // Prompts user to choose what to solve for
+    while (!solving) {  
+        cout <<"\t1. face value\n\t2. present value\n\t3. interest rate\n>> " << endl;
+        cin >> solve;
 
-void yieldMaturity() {}
+        if (solve != 1 || solve != 2 || solve != 3) { 
+            cout << "please choose a valid number\n" << endl;
+        }else { 
+            solving = true; 
+        }
+    }
+
+    // Makes interestRateProblem object to store variables and solve
+    interestRateProblem current = interestRateProblem();
+    while (solving) {
+
+    }
+
+    return current;
+}
+
+interestRateProblem discountInterest() {}
+
+interestRateProblem presentValue() {} 
+
+interestRateProblem yieldMaturity() {}
 
 // main function of the program handles user input and direction
 int main() {
-    string topic = "";
     bool exit = false;
+    string topic = "";
+    vector<interestRateProblem> solvedIR;
+
     cout << "Welcome to the Economic Calculator!\n\nHow can I help you today?\n" << endl;
 
     // main loop of the program prompts user until they want to exit
@@ -37,10 +65,10 @@ int main() {
         
         if (topic == "help" || topic == "Help" || topic == "HELP") { help(); }
         else if (topic == "exit") { exit = true; }
-        else if (topic == "ci") {}
-        else if (topic == "di") {}
-        else if (topic == "pv") {}
-        else if (topic == "ytm") {}
+        else if (topic == "ci") { solvedIR.push_back(compoundInterest()); }
+        else if (topic == "di") { solvedIR.push_back(discountInterest()); }
+        else if (topic == "pv") { solvedIR.push_back(presentValue()); }
+        else if (topic == "ytm") { solvedIR.push_back(yieldMaturity()); }
         else { cout << "Please use one of the valid commands\ntype 'help' for full list of commands\n" << endl; }
     }
 
