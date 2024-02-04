92cdcd05b0fc7dff5d645b31a2868e9285244010
Tristan Mangin <tw.mangin@gmail.com>
2024-02-03 20:58:54 -0500

    "help" Command Implementation

--- main.cpp
+++ main.cpp
@@ -7,42 +7,41 @@ using namespace std;
 
 string COMMANDS[] = {"help", "exit", "ci", "di", "pv", "ytm"};
 
-/*
-Called when the user chooses to solve an interest rate problem
-    - Takes user input to choose what is being solved for 
-    - Adds variables to a class and performs operations within the class
-*/
-
 // Outputs Instructions and the list of commands to the terminal
 void help() {
-    cout << "Prints instructions and list of commands" << endl;
+    cout << "\t1. exit\t- terminate the program\n" <<
+    "\t2. ci\t- solve for compounding interest\n" <<
+    "\t3. di\t- solve for discounting interest\n" <<
+    "\t4. pv\t- solve for present value\n" <<
+    "\t5. ytm\t- solve for yield to maturity\n"<< endl;
 }
 
-void interestRates() {
+void compoundInterest() {}
 
-}
+void discountInterest() {}
 
-void monetaryPolicy() {
+void presentValue() {} 
 
-}
-
-void fiscalPolicy() {
-    
-}
+void yieldMaturity() {}
 
 // main function of the program handles user input and direction
 int main() {
     string topic = "";
     bool exit = false;
-    cout << "Welcome to the Economic Calculator!\n\nHow can I help you today?" << endl;
+    cout << "Welcome to the Economic Calculator!\n\nHow can I help you today?\n" << endl;
 
     // main loop of the program prompts user until they want to exit
     while (!exit) {
-        cout << ">>";
+        cout << ">> ";
         cin >> topic;
         
         if (topic == "help" || topic == "Help" || topic == "HELP") { help(); }
         else if (topic == "exit") { exit = true; }
+        else if (topic == "ci") {}
+        else if (topic == "di") {}
+        else if (topic == "pv") {}
+        else if (topic == "ytm") {}
+        else { cout << "Please use one of the valid commands\ntype 'help' for full list of commands\n" << endl; }
     }
 
     return 0;
