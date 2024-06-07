30146c0cb5a20495b2e5ca21850514b49c935a17
Tristan Mangin <tw.mangin@gmail.com>
2024-06-07 12:19:31 -0400

    Create Main.java

---
+++ Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import java.util.Scanner;
+
+/**
+ * This class handles the entire running of the program. It will take user input and call needed 
+ * classes.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in); 
+    }
+}
