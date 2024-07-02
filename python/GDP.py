4abff9feba9150c79d5d5dbd9251e0c8a67b6fa0
Tristan Mangin <tw.mangin@gmail.com>
2024-07-02 09:59:09 -0400

    Update GDP.py

--- python/GDP.py
+++ python/GDP.py
@@ -51,3 +51,58 @@ class GDP:
             self.government = 0
             self.netExports = 0
             self.gdp = 0
+    
+    '''
+    ACCESSORS
+    '''
+
+    # returns the string representation of the basic equation
+    def getEquation(self):
+        return "GDP = N + I + G + NX"
+    
+    # Returns the full equation represented by the current object
+    def getFilledEquation(self):
+        return "{:.2f} = {:.2f} + {:.2f} + {:.2f} + {:.2f}".format(self.gdp, self.consumption, self.investment, self.government, self.netExports)
+
+    def getName(self):
+        return self.name
+    
+    def getConsumption(self):
+        return self.consumption
+    
+    def getInvestment(self):
+        return self.investment
+    
+    def getGovernment(self):
+        return self.government
+    
+    def getNetExport(self):
+        return self.netExports
+
+    def getGDP(self):
+        return self.gdp
+    
+    '''
+    MODIFIERS
+    '''
+
+    def setConsumption(self, c):
+        self.gdp = self.gdp - self.consumption + c
+        self.consumption = c
+
+    def setInvestment(self, i):
+        self.gdp = self.gdp - self.investment + i
+        self.investment = i
+
+    def setGovernment(self, g):
+        self.gdp = self.gdp - self.government + g
+        self.government = g
+    
+    def setNetExports(self, nx):
+        self.gdp = self.gdp - self.netExports + nx
+        self.netExports = nx
+
+    def clear(self):
+        self.price = 0
+        self.quantity = 0
+        self.revenue = 0
